2e1ee75040abc372d084eda7b955fc8d6bac5ec6
MaltePetersen <petersen-malte@web.de>
2019-09-23 17:08:42 +0200

    Beginnign implementaions of PlayerProvider

--- wicolo/lib/main.dart
+++ wicolo/lib/main.dart
@@ -31,6 +31,13 @@ class Counter with ChangeNotifier {
     notifyListeners();
   }
 }
+class Player with ChangeNotifier {
+  List<String> players = List();
+  void addPlayer(String player){
+    players.add(player);
+  }
+  }
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -50,42 +57,15 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyHomePage  extends StatelessWidget {
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter = _counter + 2;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
