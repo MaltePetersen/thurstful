9c6159b8773d7fd9c93151cbd10edeaaab01961c
MaltePetersen <petersen-malte@web.de>
2019-09-23 23:14:29 +0200

    Implemented Provider Class Player for Global ChangeNotifier

--- wicolo/lib/main.dart
+++ wicolo/lib/main.dart
@@ -1,43 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 void main() {
   runApp(
     // Provide the model to all widgets within the app. We're using
     // ChangeNotifierProvider because that's a simple way to rebuild
     // widgets when a model changes. We could also just use
-    // Provider, but then we would have to listen to Counter ourselves.
+    // Provider, but then we would have to listen to Spieler ourselves.
     //
     // Read Provider's docs to learn about all the available providers.
     ChangeNotifierProvider(
       // Initialize the model in the builder. That way, Provider
-      // can own Counter's lifecycle, making sure to call `dispose`
+      // can own Spieler's lifecycle, making sure to call `dispose`
       // when not needed anymore.
-      builder: (context) => Counter(),
+      builder: (context) => Spieler(),
       child: MyApp(),
     ),
   );
 }
+
 /// Simplest possible model, with just one field.
 ///
-/// [ChangeNotifier] is a class in `flutter:foundation`. [Counter] does
+/// [ChangeNotifier] is a class in `flutter:foundation`. [Spieler] does
 /// _not_ depend on Provider.
-class Counter with ChangeNotifier {
+class Spieler with ChangeNotifier {
   int value = 0;
+  List<String> players = List();
+  String player = "";
 
   void increment() {
     value += 1;
     notifyListeners();
   }
-}
-class Player with ChangeNotifier {
-  List<String> players = List();
-  void addPlayer(String player){
+
+  void addPlayer(String player) {
     players.add(player);
   }
-  }
 
+  void changePlayer(String player) {
+    this.player = player;
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -53,7 +56,7 @@ class MyApp extends StatelessWidget {
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
+        // Notice that the Spieler didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.amber,
       ),
@@ -62,21 +65,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> players = List();
+  final myController = TextEditingController();
 
-class MyHomePage  extends StatelessWidget {
-
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // by the _incrementSpieler method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-                backgroundColor: Colors.lightBlue,
+      backgroundColor: Colors.lightBlue,
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -100,42 +114,53 @@ class MyHomePage  extends StatelessWidget {
           children: <Widget>[
             Text(
               'Wicolo',
-            style: TextStyle(color: Colors.white,        fontSize: 40,
- ),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+              ),
             ),
-      TextFormField(
-  decoration: InputDecoration(
-    labelText: 'Spieler 1',
-    labelStyle: TextStyle(color: Colors.white),
-  ),
-),
-          TextFormField(
-  decoration: InputDecoration(
-    labelText: 'Spieler 2',
-    labelStyle: TextStyle(color: Colors.white),
-  ),
-),
-      TextFormField(
-  decoration: InputDecoration(
-    labelText: 'Spieler 3',
-        labelStyle: TextStyle(color: Colors.white)
-  ),
-),   const SizedBox(height: 30),
-        RaisedButton(
-          child: const Text(
-            'Go to Second',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-          ),
-          onPressed: () {
-            // Pushs the SecondScreen widget onto the navigation stack
-            Navigator
-                .of(context)
-                .push(MaterialPageRoute(builder: (_) => CounterPage()));
-          },
-        ),
-           Consumer<Counter>(
-              builder: (context, counter, child) => Text(
-                '${counter.value}',
+            TextFormField(
+              controller: myController,
+              decoration: InputDecoration(
+                labelText: 'Spieler 1',
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextFormField(
+              controller: myController,
+              decoration: InputDecoration(
+                labelText: 'Spieler 2',
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextFormField(
+              controller: myController,
+              decoration: InputDecoration(
+                  labelText: 'Spieler 3',
+                  labelStyle: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 30),
+            RaisedButton(
+              child: const Text(
+                'Go to Second',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              onPressed: () {
+                Provider.of<Spieler>(context, listen: false)
+                    .changePlayer(myController.text);
+                // Pushs the SecondScreen widget onto the navigation stack
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => SpielerPage()));
+              },
+            ),
+            Consumer<Spieler>(
+                builder: (context, player, child) => Text(
+                      '${player.player}',
+                      style: Theme.of(context).textTheme.display1,
+                    )),
+            Consumer<Spieler>(
+              builder: (context, Spieler, child) => Text(
+                '${Spieler.value}',
                 style: Theme.of(context).textTheme.display1,
               ),
             ),
@@ -145,24 +170,8 @@ class MyHomePage  extends StatelessWidget {
     );
   }
 }
-class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Screen'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: const Text('Go to First'),
-          // Pops Second Screen off the navigation stack
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-    );
-  }
-}
-class CounterPage extends StatelessWidget {
+
+class SpielerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -170,22 +179,22 @@ class CounterPage extends StatelessWidget {
         title: Text('Flutter Demo Home Page'),
       ),
       body: Center(
-              child: Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-              RaisedButton(
-          child: const Text('Go to First'),
-          // Pops Second Screen off the navigation stack
-          onPressed: () => Navigator.of(context).pop(),
-        ),
+            RaisedButton(
+              child: const Text('Go to First'),
+              // Pops Second Screen off the navigation stack
+              onPressed: () => Navigator.of(context).pop(),
+            ),
             Text('You have pushed the button this many times:'),
             // Consumer looks for an ancestor Provider widget
-            // and retrieves its model (Counter, in this case).
+            // and retrieves its model (Spieler, in this case).
             // Then it uses that model to build widgets, and will trigger
             // rebuilds if the model is updated.
-            Consumer<Counter>(
-              builder: (context, counter, child) => Text(
-                '${counter.value}',
+            Consumer<Spieler>(
+              builder: (context, Spieler, child) => Text(
+                '${Spieler.value}',
                 style: Theme.of(context).textTheme.display1,
               ),
             ),
@@ -201,13 +210,12 @@ class CounterPage extends StatelessWidget {
         // By using `listen: false` below, we are disabling that
         // behavior. We are only calling a function here, and so we don't care
         // about the current value. Without `listen: false`, we'd be rebuilding
-        // the whole MyHomePage whenever Counter notifies listeners.
+        // the whole MyHomePage whenever Spieler notifies listeners.
         onPressed: () =>
-            Provider.of<Counter>(context, listen: false).increment(),
+            Provider.of<Spieler>(context, listen: false).increment(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
-      
     );
   }
 }
